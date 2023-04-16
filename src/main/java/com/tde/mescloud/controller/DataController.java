4b373165b9559f9d1bdf042c4871d4c65de6f74a
antoniopmeloferreira <antoniopmeloferreira@gmail.com>
2023-04-16 11:48:24 +0100

    deleted

--- src/main/java/com/tde/mescloud/controller/DataController.java
+++ src/main/java/com/tde/mescloud/controller/DataController.java
@@ -1,28 +1,23 @@
 package com.tde.mescloud.controller;
 
-import com.tde.mescloud.Constants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.tde.mescloud.Constants.CELEBRATION_TIME;
-
 @RestController
 @RequestMapping("/")
 public class DataController {
 
-    private Constants constants;
-
     @GetMapping
     public ResponseEntity<String> healthCheck() {
-        return new ResponseEntity<>(CELEBRATION_TIME, HttpStatus.OK);
+        return new ResponseEntity<>("YEAH", HttpStatus.OK);
     }
 
     @GetMapping("/version")
     public String version() {
-        return constants.getVersion();
+        return "Version MES 0.0.1";
     }
 
     @GetMapping("/string")
