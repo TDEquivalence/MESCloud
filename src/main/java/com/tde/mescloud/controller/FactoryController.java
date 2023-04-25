7abbb5cc15d02feb0a1e1f32c23e60947c2318b4
tdedevelopment <99899252+tdedevelopment@users.noreply.github.com>
2023-04-25 23:11:46 +0100

    Update FactoryController.java

--- src/main/java/com/tde/mescloud/controller/FactoryController.java
+++ src/main/java/com/tde/mescloud/controller/FactoryController.java
@@ -5,7 +5,7 @@ import com.tde.mescloud.repository.FactoryRepository;
 import org.aspectj.runtime.reflect.Factory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
