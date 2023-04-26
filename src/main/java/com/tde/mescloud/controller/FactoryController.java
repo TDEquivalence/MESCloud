afc2994c7a452e17afeea806f58ecf676c78b071
tdedevelopment <99899252+tdedevelopment@users.noreply.github.com>
2023-04-26 09:42:20 +0100

    Update FactoryController.java

--- src/main/java/com/tde/mescloud/controller/FactoryController.java
+++ src/main/java/com/tde/mescloud/controller/FactoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/factory")
+@RequestMapping("factory")
 public class FactoryController {
 
     private FactoryRepository factoryRepository;
