dc19814581840fa4e1b63f36e466edf35baed3c1
antoniomelo <antoniopmeloferreira@gmail.com>
2023-05-07 23:23:34 +0100

    MC-50 Security

---
+++ src/main/java/com/tde/mescloud/security/exception/EmailExistException.java
@@ -0,0 +1,8 @@
+package com.tde.mescloud.security.exception;
+
+public class EmailExistException extends Exception{
+
+    public EmailExistException(String message) {
+        super(message);
+    }
+}
