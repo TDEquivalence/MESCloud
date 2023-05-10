33f80ecefaebf039e27a9922e61779ae248c4047
antoniopmeloferreira <antoniopmeloferreira@gmail.com>
2023-05-10 18:04:41 +0100

    Add date JsonFormat

--- src/main/java/com/tde/mescloud/security/model/auth/AuthenticationResponse.java
+++ src/main/java/com/tde/mescloud/security/model/auth/AuthenticationResponse.java
@@ -1,5 +1,6 @@
 package com.tde.mescloud.security.model.auth;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tde.mescloud.security.role.Role;
 import lombok.*;
 
@@ -16,6 +17,7 @@ public class AuthenticationResponse {
     private String username;
     private String firstName;
     private String lastName;
-    private Date createdAt;
     private Role role;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Europe/Lisbon")
+    private Date createdAt;
 }
