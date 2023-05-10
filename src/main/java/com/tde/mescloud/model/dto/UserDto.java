9f428aa0d028de8aed4f1fea51c82daa59723889
antoniopmeloferreira <antoniopmeloferreira@gmail.com>
2023-05-10 18:06:31 +0100

    date jsonformat

--- src/main/java/com/tde/mescloud/model/dto/UserDto.java
+++ src/main/java/com/tde/mescloud/model/dto/UserDto.java
@@ -1,10 +1,13 @@
 package com.tde.mescloud.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tde.mescloud.security.role.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Setter
 @Getter
@@ -18,4 +21,6 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private Role role;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Europe/Lisbon")
+    private Date createdAt;
 }
