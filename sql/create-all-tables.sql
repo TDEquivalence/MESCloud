3e8ee88859fcd5a280f9d6eebab35d8a467428a8
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2023-05-11 15:41:56 +0100

    Update postman collection and sql scripts

--- sql/create-all-tables.sql
+++ sql/create-all-tables.sql
@@ -8,6 +8,7 @@ DROP TABLE IF EXISTS equipment_output;
 DROP TABLE IF EXISTS equipment_output_alias;
 DROP TABLE IF EXISTS section;
 DROP TABLE IF EXISTS factory;
+DROP TABLE IF EXISTS token;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -25,6 +26,17 @@ CREATE TABLE users (
   is_not_locked BOOLEAN,
 
   PRIMARY KEY(id)
+);
+
+CREATE TABLE token (
+  id SERIAL PRIMARY KEY,
+  token VARCHAR(255) NOT NULL,
+  token_type VARCHAR(10) NOT NULL,
+  expired BOOLEAN,
+  revoked BOOLEAN,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE factory (
