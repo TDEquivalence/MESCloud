75448cfab281628bdeb3c078517101e8efa490c2
antoniopmeloferreira <antoniopmeloferreira@gmail.com>
2023-05-12 19:22:19 +0100

    add cross origin https

--- src/main/java/com/tde/mescloud/MescloudApplication.java
+++ src/main/java/com/tde/mescloud/MescloudApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = {"http://staging.mescloud.pt", "http://staging.mescloud.pt"})
+@CrossOrigin(origins = {"http://staging.mescloud.pt", "http://staging.mescloud.pt", "https://www.mescloud.pt"})
 @SpringBootApplication
 public class MescloudApplication {
 
