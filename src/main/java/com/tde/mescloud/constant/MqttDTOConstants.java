dbdd16075b20adcd5da7c6bdc9e81cd9691a421f
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2023-07-12 23:33:10 +0100

    MC-150 - Change jsonType names and unify plc response structure

--- src/main/java/com/tde/mescloud/constant/MqttDTOConstants.java
+++ src/main/java/com/tde/mescloud/constant/MqttDTOConstants.java
@@ -9,7 +9,7 @@ public class MqttDTOConstants {
 
     public static final String JSON_TYPE_PROPERTY_NAME = "jsonType";
     public static final String PRODUCTION_ORDER_DTO_NAME = "ProductionOrder";
-    public static final String PRODUCTION_ORDER_INIT_DTO_NAME = "ProductionOrderInit";
+    public static final String PRODUCTION_ORDER_RESPONSE_DTO_NAME = "ProductionOrderResponse";
     public static final String COUNTING_RECORD_DTO_NAME = "ProductionCount";
     public static final String HAS_RECEIVED_DTO_NAME = "Received";
     public static final String EQUIPMENT_CONFIG_DTO_NAME = "Configuration";
--- src/main/java/com/tde/mescloud/protocol/ProductionOrderInitProcess.java
+++ src/main/java/com/tde/mescloud/protocol/ProductionOrderInitProcess.java
@@ -1,17 +1,12 @@
 package com.tde.mescloud.protocol;
 
 import com.tde.mescloud.constant.MqttDTOConstants;
-import com.tde.mescloud.model.dto.CounterRecordDto;
-import com.tde.mescloud.model.dto.CountingEquipmentDto;
 import com.tde.mescloud.model.dto.EquipmentCountsMqttDto;
 import com.tde.mescloud.service.CounterRecordService;
 import com.tde.mescloud.service.CountingEquipmentService;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @Log
@@ -42,6 +37,6 @@ public class ProductionOrderInitProcess extends AbstractMesProtocolProcess<Equip
 
     @Override
     public String getMatchingDTOName() {
-        return MqttDTOConstants.PRODUCTION_ORDER_INIT_DTO_NAME;
+        return MqttDTOConstants.PRODUCTION_ORDER_RESPONSE_DTO_NAME;
     }
 }
