74687cf0149c69a86a4c23f65d7c1bea4e108b3e
antoniopmeloferreira <antoniopmeloferreira@gmail.com>
2023-07-19 23:30:33 +0100

    MC-180 - The production Order button conclusion doesn't change the state

--- src/main/java/com/tde/mescloud/service/ProductionOrderServiceImpl.java
+++ src/main/java/com/tde/mescloud/service/ProductionOrderServiceImpl.java
@@ -80,8 +80,14 @@ public class ProductionOrderServiceImpl implements ProductionOrderService {
             log.severe(() -> String.format("Unable to publish Order Completion to PLC for equipment [%s]", equipmentId));
         }
 
-        ProductionOrderDto productionOrder = converter.toDto(productionOrderEntityOpt.get());
-        return Optional.of(productionOrder);
+        try {
+            Thread.sleep(250);
+            ProductionOrderDto productionOrder = converter.toDto(productionOrderEntityOpt.get());
+            return Optional.of(productionOrder);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
