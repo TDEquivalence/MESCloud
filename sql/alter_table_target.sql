85d30f8b833056e8e744f25556a936e605e7c5b8
antoniopmeloferreira <antoniopmeloferreira@gmail.com>
2023-09-26 22:48:11 +0100

    add script target properties

---
+++ sql/alter_table_target.sql
@@ -0,0 +1,11 @@
+ALTER TABLE counting_equipment
+RENAME COLUMN performance TO performance_target;
+
+ALTER TABLE counting_equipment
+RENAME COLUMN quality TO quality_target;
+
+ALTER TABLE counting_equipment
+RENAME COLUMN availability TO availability_target;
+
+ALTER TABLE counting_equipment
+RENAME COLUMN equipment_effectiveness TO overall_equipment_effectiveness;
