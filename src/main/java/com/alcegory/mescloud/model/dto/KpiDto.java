51f889b8ccb6cdded679255dedec9f1af972895c
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2023-10-05 22:46:48 +0100

    MES-292 - Improve models logic

--- src/main/java/com/alcegory/mescloud/model/dto/KpiDto.java
+++ src/main/java/com/alcegory/mescloud/model/dto/KpiDto.java
@@ -4,26 +4,34 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.java.Log;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@Log
 public class KpiDto {
 
     private Double dividend;
     private Double divider;
-    private Double value;
 
     public KpiDto(Integer dividend, Integer divider) {
         this.dividend = Double.valueOf(dividend);
         this.divider = Double.valueOf(divider);
-        this.value = this.dividend / this.divider;
     }
 
     public KpiDto(Double dividend, Double divider) {
         this.dividend = dividend;
         this.divider = divider;
-        this.value = this.dividend / this.divider;
+    }
+
+    public Double getValue() {
+
+        if (dividend == null || dividend == 0 || divider == null || divider == 0) {
+            log.warning(String.format("Unable to calculate value: cannot divide dividend [%s] by the divisor [%s]", dividend, divider));
+            return null;
+        }
+
+        return this.dividend / this.divider;
     }
 }
