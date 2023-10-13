481f3ff6d44c9e195026c1d7773ce7cef0bb67bd
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2023-10-13 15:13:09 +0100

    MES-200 - API route fix

--- src/main/java/com/alcegory/mescloud/api/rest/AlarmRecordController.java
+++ src/main/java/com/alcegory/mescloud/api/rest/AlarmRecordController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/alarm")
+@RequestMapping("/api/alarm-record")
 @AllArgsConstructor
 public class AlarmRecordController {
 
