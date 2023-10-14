bab4953c304f04ccefb9d76a7b72cd88c7ca3d19
Luís Guilherme Ferreira <56452762+lguilhermef@users.noreply.github.com>
2023-10-14 22:41:21 +0100

    MES-341 - AlarmRecord counts implementation (#62)

--- src/main/java/com/alcegory/mescloud/repository/AlarmRecordRepository.java
+++ src/main/java/com/alcegory/mescloud/repository/AlarmRecordRepository.java
@@ -1,5 +1,6 @@
 package com.alcegory.mescloud.repository;
 
+import com.alcegory.mescloud.model.entity.AlarmRecordCounts;
 import com.alcegory.mescloud.model.entity.AlarmRecordEntity;
 import com.alcegory.mescloud.model.filter.AlarmRecordFilter;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface AlarmRecordRepository extends JpaRepository<AlarmRecordEntity, Long> {
 
     List<AlarmRecordEntity> findAllByFilter(AlarmRecordFilter filter);
+
+    AlarmRecordCounts getAlarmRecordCounts(AlarmRecordFilter filter);
 }
--- src/main/java/com/alcegory/mescloud/repository/AlarmRecordRepositoryImpl.java
+++ src/main/java/com/alcegory/mescloud/repository/AlarmRecordRepositoryImpl.java
@@ -1,7 +1,12 @@
 package com.alcegory.mescloud.repository;
 
+import com.alcegory.mescloud.constant.AlarmStatus;
+import com.alcegory.mescloud.model.entity.AlarmRecordCounts;
 import com.alcegory.mescloud.model.entity.AlarmRecordEntity;
 import com.alcegory.mescloud.model.filter.AlarmRecordFilter;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +16,21 @@ public class AlarmRecordRepositoryImpl extends AbstractFilterRepository<AlarmRec
 
     public List<AlarmRecordEntity> findAllByFilter(AlarmRecordFilter filter) {
         return super.findAllWithFilter(filter, AlarmRecordEntity.class);
+    }
+
+    public AlarmRecordCounts getAlarmRecordCounts(AlarmRecordFilter filter) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<AlarmRecordCounts> query = cb.createQuery(AlarmRecordCounts.class);
+        Root<AlarmRecordEntity> root = query.from(AlarmRecordEntity.class);
+
+        query.select(cb.construct(AlarmRecordCounts.class,
+                cb.count(root),
+                cb.sum(cb.<Long>selectCase()
+                        .when(cb.equal(root.get("status"), AlarmStatus.ACTIVE), 1L)
+                        .otherwise(0L)
+                )
+        ));
+
+        return entityManager.createQuery(query).getSingleResult();
     }
 }
