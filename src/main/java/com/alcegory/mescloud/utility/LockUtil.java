ebc225a62d0de4ac4bb7ed51ce349e5825d14f02
antoniopmeloferreira <antoniopmeloferreira@gmail.com>
2023-10-15 16:37:45 +0100

    MES-275 - Production Order Conclusion

--- src/main/java/com/alcegory/mescloud/utility/LockUtil.java
+++ src/main/java/com/alcegory/mescloud/utility/LockUtil.java
@@ -24,18 +24,20 @@ public class LockUtil {
         Lock lock = lockMap.computeIfAbsent(equipmentCode, k -> new ReentrantLock());
         lock.lock();
         try {
-            CountDownLatch latch = new CountDownLatch(1);
+            CountDownLatch latch = new CountDownLatch(2);
             CountDownLatch existingLatch = locks.putIfAbsent(equipmentCode, latch);
             if (existingLatch != null) {
                 throw new IllegalStateException("Lock already acquired for equipmentCode: " + equipmentCode);
             }
         } catch (IllegalStateException e) {
             log.severe(() -> String.format("Failed acquire lock for equipment with code [%s]", equipmentCode));
+            lock.unlock();
         }
     }
 
 
-    public void unlock(String equipmentCode) {
+    public void unlockToCount1(String equipmentCode) {
+        log.info(() -> String.format("LOCK count to 1 for equipment with code [%s]", equipmentCode));
         Lock lock = lockMap.get(equipmentCode);
         CountDownLatch latch = locks.get(equipmentCode);
 
@@ -45,6 +47,24 @@ public class LockUtil {
 
         try {
             latch.countDown();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void unlockToCount0(String equipmentCode) {
+        log.info(() -> String.format("LOCK count to 0 for equipment with code [%s]", equipmentCode));
+        Lock lock = lockMap.get(equipmentCode);
+        CountDownLatch latch = locks.get(equipmentCode);
+
+        if (lock == null || latch == null) {
+            throw new IllegalStateException("Lock not found for equipmentCode: " + equipmentCode);
+        }
+
+        try {
+            while (latch.getCount() > 0) {
+                latch.countDown();
+            }
         } finally {
             lock.unlock();
         }
