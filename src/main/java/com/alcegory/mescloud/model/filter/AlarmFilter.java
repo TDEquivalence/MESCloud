10d0dc8a280598a45087888b8bcd4bddb2b81a14
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2023-10-30 00:11:08 +0000

    MES-382 - Add Alarm ordering properties and invert alarm config index

--- src/main/java/com/alcegory/mescloud/model/filter/AlarmFilter.java
+++ src/main/java/com/alcegory/mescloud/model/filter/AlarmFilter.java
@@ -20,7 +20,9 @@ public class AlarmFilter extends AbstractFilter<AlarmFilter.Property> {
 
         STATUS("status", FilterDataTypeOperation.STRING_EQUAL),
         START_DATE("startDate", CREATED_AT_PROP, FilterDataTypeOperation.DATE_GREATER_OR_EQUAL),
-        END_DATE("endDate", CREATED_AT_PROP, FilterDataTypeOperation.DATE_LESS_OR_EQUAL);
+        END_DATE("endDate", CREATED_AT_PROP, FilterDataTypeOperation.DATE_LESS_OR_EQUAL),
+        COMPLETED_AT("completedAt", FilterDataTypeOperation.DATE_LESS_OR_EQUAL),
+        RECOGNIZED_AT("recognizedAt", FilterDataTypeOperation.DATE_LESS_OR_EQUAL);
 
         Property(String name, FilterDataTypeOperation dataTypeOperation) {
             this.name = name;
