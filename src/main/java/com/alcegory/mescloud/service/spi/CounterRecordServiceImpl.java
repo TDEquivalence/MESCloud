99663ce427b3d52dee67d01938c6d9042fbd487a
antoniopmeloferreira <antoniopmeloferreira@gmail.com>
2023-12-04 23:52:24 +0000

    fix null pointer exception

--- src/main/java/com/alcegory/mescloud/service/spi/CounterRecordServiceImpl.java
+++ src/main/java/com/alcegory/mescloud/service/spi/CounterRecordServiceImpl.java
@@ -125,7 +125,7 @@ public class CounterRecordServiceImpl implements CounterRecordService {
         //TODO: we have to check if this validation is correct, considering we can have counter records without PO.s
         CounterRecordEntity lastPersistedCount = getLastPersistedCount(counterRecord);
 
-        if (counterRecord.getProductionOrder() != null) {
+        if (lastPersistedCount != null && counterRecord.getProductionOrder() != null) {
             setComputedValue(counterRecord, lastPersistedCount);
         }
 
