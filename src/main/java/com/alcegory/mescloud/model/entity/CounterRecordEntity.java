70f079a841971cfe9ccde56092b034a88303dc61
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2024-01-10 02:14:40 +0000

    bug/MES-392 - Set counter record incrementActiveTime as an Integer to allow null values

--- src/main/java/com/alcegory/mescloud/model/entity/CounterRecordEntity.java
+++ src/main/java/com/alcegory/mescloud/model/entity/CounterRecordEntity.java
@@ -30,5 +30,5 @@ public class CounterRecordEntity {
     private Boolean isValidForProduction;
     private int activeTime;
     private int computedActiveTime;
-    private int incrementActiveTime;
+    private Integer incrementActiveTime;
 }
