0d03ad5282b41e0441ba9d0b044f492d42b0e482
antoniopmeloferreira <antoniopmeloferreira@gmail.com>
2024-02-07 16:34:29 +0000

    Refactor Production Order summary

--- sql/version/1.0.0/0004_alter_production_summary_view.sql
+++ sql/version/1.0.0/0004_alter_production_summary_view.sql
@@ -4,3 +4,7 @@ FROM production_order po
 LEFT JOIN counter_record_production_conclusion crpc ON po.id = crpc.production_order_id
 WHERE po.is_completed = true AND crpc.is_valid_for_production = true
 GROUP BY po.id;
+
+INSERT INTO audit_script (run_date, process, version, schema)
+VALUES
+    (CURRENT_DATE, '0004_alter_production_summary_view.sql', '1.0.0', '1.0.0_0004');
