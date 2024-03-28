49626a1f088299865ad48e8f8e29d3184f536085
antoniopmelo <antoniopmeloferreira@gmail.com>
2024-03-28 12:34:54 +0000

    MES-484 - Change response in login

--- src/main/java/com/alcegory/mescloud/azure/controller/CorkDefectController.java
+++ src/main/java/com/alcegory/mescloud/azure/controller/CorkDefectController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api")
 public class CorkDefectController {
 
     private final PublicContainerService publicContainerService;
