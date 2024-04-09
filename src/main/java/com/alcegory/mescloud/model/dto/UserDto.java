947d32adf6356c11eab9b766900e763a337f6399
antoniopmelo <antoniopmeloferreira@gmail.com>
2024-04-09 18:01:37 +0100

    MES-473 - Authorizations enable permissions

--- src/main/java/com/alcegory/mescloud/model/dto/UserDto.java
+++ src/main/java/com/alcegory/mescloud/model/dto/UserDto.java
@@ -2,6 +2,7 @@ package com.alcegory.mescloud.model.dto;
 
 import com.alcegory.mescloud.security.model.Role;
 import com.alcegory.mescloud.security.model.SectionRole;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class UserDto {
     private String username;
     private String firstName;
     private String lastName;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String email;
     private Role role;
