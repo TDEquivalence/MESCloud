f8a28f4be6b1e9da01accac342fa21ae60527619
antoniopmelo <antoniopmeloferreira@gmail.com>
2024-04-09 12:06:07 +0100

    MES-473 - Authorizations enable permissions

--- sql/version/1.0.1/0005_insert_data_role.sql
+++ sql/version/1.0.1/0005_insert_data_role.sql
@@ -1,17 +1,16 @@
 INSERT INTO role (name) VALUES ('SUPER_ADMIN'), ('ADMIN'), ('ANALYST'), ('OPERATOR');
 
 INSERT INTO user_role (user_id, role_id, section_id) VALUES
-(1, 2, 1),
-(2, 2, 1),
-(3, 2, 1),
-(4, 2, 1),
-(5, 2, 1),
-(6, 2, 1),
-(7, 2, 1),
-(8, 2, 1),
-(9, 2, 1),
-(10, 2, 1),
-(11, 2, 1);
+(1, 1, 1),
+(2, 1, 1),
+(4, 4, 1),
+(5, 4, 1),
+(6, 1, 1),
+(1, 1, 2),
+(2, 1, 2),
+(4, 4, 2),
+(5, 4, 2),
+(6, 1, 1);
 
 INSERT INTO audit_script (run_date, process, version, schema)
 VALUES
