0fc9b99a1b1e702987df13e57a9085dc968cc90a
antoniopmelo <antoniopmeloferreira@gmail.com>
2024-04-17 16:40:35 +0100

    MES-522 - Update JWT Token Logic for Login and Implement Refresh Token

--- src/main/java/com/alcegory/mescloud/security/service/LogoutService.java
+++ src/main/java/com/alcegory/mescloud/security/service/LogoutService.java
@@ -1,6 +1,5 @@
 package com.alcegory.mescloud.security.service;
 
-import com.alcegory.mescloud.security.constant.SecurityConstant;
 import com.alcegory.mescloud.security.model.token.TokenEntity;
 import com.alcegory.mescloud.security.repository.TokenRepository;
 import jakarta.servlet.http.Cookie;
@@ -10,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
+
+import static com.alcegory.mescloud.security.constant.SecurityConstant.COOKIE_REFRESH_TOKEN_NAME;
+import static com.alcegory.mescloud.security.constant.SecurityConstant.COOKIE_TOKEN_NAME;
 
 @Service
 @RequiredArgsConstructor
@@ -23,15 +25,15 @@ public class LogoutService implements LogoutHandler {
         Cookie[] cookies = request.getCookies();
         final String jwtToken;
 
-        if(cookies == null || !jwtTokenService.isTokenInCookie(cookies)) {
+        if (cookies == null || !jwtTokenService.isTokenInCookie(cookies)) {
             return;
         }
 
-        jwtToken  = jwtTokenService.getJwtTokenFromCookie(cookies);
+        jwtToken = jwtTokenService.getJwtTokenFromCookie(cookies);
         removeCookie(request, response);
         TokenEntity storedToken = tokenRepository.findByToken(jwtToken)
                 .orElse(null);
-        if(storedToken != null) {
+        if (storedToken != null) {
             storedToken.setExpired(true);
             storedToken.setRevoked(true);
             tokenRepository.save(storedToken);
@@ -43,7 +45,7 @@ public class LogoutService implements LogoutHandler {
 
         if (cookies != null) {
             for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(SecurityConstant.COOKIE_TOKEN_NAME)) {
+                if (cookie.getName().equals(COOKIE_TOKEN_NAME) || cookie.getName().equals(COOKIE_REFRESH_TOKEN_NAME)) {
                     cookie.setMaxAge(0);
                     cookie.setPath("/");
                     response.addCookie(cookie);
