4c54b462b1a5792b4739e35ccf6ae7615d6a2b12
antoniopmelo <antoniopmeloferreira@gmail.com>
2024-05-02 18:28:28 +0100

    refactor image occurences

--- src/main/java/com/alcegory/mescloud/azure/controller/CorkDefectController.java
+++ src/main/java/com/alcegory/mescloud/azure/controller/CorkDefectController.java
@@ -38,6 +38,8 @@ public class CorkDefectController {
         } catch (ImageAnnotationException ex) {
             ErrorResponse errorResponse = new ErrorResponse("Internal server error occurred while saving image annotation");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
         }
     }
 
